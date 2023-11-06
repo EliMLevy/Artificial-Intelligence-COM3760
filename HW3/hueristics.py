09e14620579be584e2a3aee0b108473ef259f2dd
Eliyahu Levy <eliyahu.levy21@gmail.com>
2023-11-06 11:57:59 -0500

    Comments

--- HW3/hueristics.py
+++ HW3/hueristics.py
@@ -1,16 +1,29 @@
 import random
 import game
 
+'''
+Default hueristic
+deterministic and not very good 
+'''
 def naive_hueristic(s):
     #Returns the heuristic value of s
     s[1] = 1  # your code here ###
     return s[1]
 
+
+'''
+Random hueristic
+non-deterministic and not very good 
+'''
 def random_hueristic(s):
     s[1] = random.randint(-5, 5)
     return s[1]
 
 
+'''
+Side grabber hueristic
+The sides are better because they are not so easily captured.
+'''
 def side_grabber_hueristic(s):
 
     mask = [0,0,0,0,0,0,0,0,0,0,0,
@@ -34,7 +47,9 @@ def side_grabber_hueristic(s):
 
     return val
 
-
+'''
+The corners are even better than the sides because they are impossible to capture
+'''
 def corner_grabber_hueristic(s):
 
     mask = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -57,6 +72,17 @@ def corner_grabber_hueristic(s):
     return val
 
 
+'''
+The corners are best and the sides are important. It is also important to try preventing the opponent 
+from getting the sides and corners. It is not strictly necessary to put the negative values in the
+spots that allow the opponent to get the sides and corners becuase the minimax algorithm will give negative
+scores to the states where the opponent gets the sides and corners. However, since the depth is limited, the 
+negative weights give us an extra level of depth for free by disincentivizing going somewhere that the 
+opponent can get a corner or side on the next turn.
+
+The middle four squares are given value because in the end game, controlling the center often turns the tide
+when the difference in pieces is close.
+'''
 def defensive_corner_grabber_hueristic(s):
 
     mask = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
